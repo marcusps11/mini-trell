df7ca9ba895133b27d1e765cfe0b9847ae1fb4db
marcusps11 <marcus@1roof.com>
2018-02-11 09:44:49 +0000

    Adding more styles

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -20,6 +20,20 @@ class Trello extends Component {
             <p>List 1</p>
           </div>
         </div>
+        <div className="list">
+          <div className="list__name">
+            <h3>Name of the list</h3>
+          </div>
+          <div className="list__item">
+            <p>List item 1</p>
+          </div>
+          <div className="list__item">
+            <p>List 2</p>
+          </div>
+          <div className = "list__item">
+            <p>List 1</p>
+          </div>
+        </div>
       </div>
     );
   }
